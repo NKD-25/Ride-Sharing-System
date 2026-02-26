9688d07bc9eae623d107a665e149b78ce595e942
Nishchal <nishchal831nkd@gmail.com>
2026-02-26 13:15:58 +0530

    updating app.js acc to routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.routes");
+const rideRoutes = require("./routes/ride.routes");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/rides", rideRoutes);
 
 module.exports = app;
