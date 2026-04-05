78cd95f4fc2405b47dc7c77e3b5fa6f3cb3f6bf9
Ishani <deviishani512@gmial.com>
2026-04-05 10:18:11 +0530

    booking routes for mongo  ...added

---
+++ src/routes/booking.routes.mongo.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+const bookingController = require("../controllers-mongo/booking.controller")
+const authMiddleware = require("../middleware/auth.middleware")
+
+router.post("/", authMiddleware, bookingController.bookRide)
+router.get("/driver", authMiddleware, bookingController.getDriverBookings)
+router.get("/rider", authMiddleware, bookingController.getRiderBookings)
+router.put("/:id", authMiddleware, bookingController.updateBookingStatus)
+router.put("/:id/cancel", authMiddleware, bookingController.cancelBookingByRider)
+
+module.exports = router
