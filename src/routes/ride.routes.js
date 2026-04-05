8049f1db1d1cc7b3ad4d91427d87bd995c2bef76
Ishani <deviishani512@gmial.com>
2026-04-05 10:19:38 +0530

    route for rides

---
+++ src/routes/ride.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const rideController = require("../controllers/ride.controller");
+const authMiddleware = require("../middleware/auth.middleware");
+
+router.post("/", authMiddleware, rideController.createRide);
+router.get("/", rideController.getAllRides);
+router.get("/suggest-price", rideController.suggestPrice);
+router.get("/:id", rideController.getRideById);
+router.put("/:id", authMiddleware, rideController.updateRide);
+router.delete("/:id", authMiddleware, rideController.deleteRide);
+
+module.exports = router;
